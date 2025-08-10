6ee207c4d7e46a07d8dc6f503e8b2300c4bcd9e9
filcher <146027639+filcherock@users.noreply.github.com>
2025-08-10 21:50:37 +0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -20,6 +20,7 @@ nasm -f bin src/boot/boot.asm -o bin/boot.bin
 echo -e "${GREEN}Compiling the kernel and programs${NC}"
 nasm -f bin src/kernel/kernel.asm -o bin/kernel.bin
 nasm -f bin program/calc.asm -o bin/calc.bin
+nasm -f bin program/blockNote.asm -o bin/blockNote.bin
 
 echo -e "${GREEN}Creating a disk image${NC}"
 dd if=/dev/zero of=img/irion.img bs=512 count=50
@@ -27,7 +28,8 @@ dd if=/dev/zero of=img/irion.img bs=512 count=50
 dd if=bin/boot.bin of=img/irion.img conv=notrunc
 dd if=bin/kernel.bin of=img/irion.img bs=512 seek=1 conv=notrunc
 dd if=bin/calc.bin of=img/irion.img bs=512 seek=5 conv=notrunc
+dd if=bin/blockNote.bin of=img/irion.img bs=512 seek=10 conv=notrunc
 
 echo -e "${GREEN}Launching QEMU...${NC}"
-qemu-system-i386 -hda img/irion.img
+qemu-system-i386 -audiodev pa,id=snd0 -machine pcspk-audiodev=snd0 -hda img/irion.img
 
